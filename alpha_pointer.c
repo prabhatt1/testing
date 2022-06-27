d92e0ca91dc0e7abbcac640707b061b419e1855c
Prabhat <prabhat1.tiwari@intel.com>
2022-06-27 06:15:44 +0100

    Committing Codes

---
+++ alpha_pointer.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void alpha(char c) {
+  char *p = &c;
+
+  while (*p <= 90) {
+    printf("%c ", *p);
+    ++*p;
+  }
+printf("\n");
+}
+
+int main() {
+  alpha(65);
+  return 0;
+}
